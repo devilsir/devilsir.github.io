e20aaa311d9cb5264514660d688afdd81959919c
quimicavisualufv <quimicavisualufv@gmail.com>
2026-09-02 12:05:57 -0300

    .

--- Sobre/js/config.js
+++ Sobre/js/config.js
@@ -2,5 +2,6 @@ window.APP_CONFIG = {
   modelPath: "assets/models/personagem.glb",
   mobileModelPath: "assets/models/personagem-mobile.glb",
   mobileMaxPixelRatio: 1.25,
-  mobileFrameRate: 30
+  mobileFrameRate: 30,
+  useDedicatedMobileModel: true
 };
